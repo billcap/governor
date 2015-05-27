f5978e6c270f7da3692d08310a02d3f7635987e5
Cheney Lin <lincheney@gmail.com>
2015-05-27 15:58:06 +1000

    make etcd host configurable from env vars

--- helpers/config.py
+++ helpers/config.py
@@ -8,6 +8,7 @@ def load_config(filename):
 
     config.setdefault('etcd', {})
     etcd = config['etcd']
+    etcd.setdefault('host', os.environ.get('ETCD_HOST', '127.0.0.1:4001'))
     etcd.setdefault('scope', '')
     etcd['scope'] = etcd['scope'].lstrip('/')
 
