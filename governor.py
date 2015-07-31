baaabf0c590bff65e6798a077b02e4e81c053fd2
Cheney Lin <lincheney@gmail.com>
2015-07-31 14:58:33 +1000

    move helpers -> governor

--- governor.py
+++ governor.py
@@ -5,17 +5,9 @@ import os
 import signal
 import sys
 import socket
-import time
-import yaml
-import subprocess
 import argparse
 
-import etcd
-
-from helpers.etcd import Client as Etcd
-from helpers.postgresql import Postgresql
-from helpers.ha import Ha
-
+from governor import Governor
 
 def sigterm_handler(signo, stack_frame):
     sys.exit()
@@ -29,75 +21,6 @@ def sigchld_handler(signo, stack_frame):
                 break
     except OSError:
         pass
-
-class Governor:
-    def __init__(self, config):
-        self.nap_time = config['loop_wait']
-
-        logging.info('waiting on etcd')
-        self.etcd = Etcd()
-        self.psql = Postgresql(config['postgresql'])
-        self.ha = Ha(self.psql, self.etcd)
-        self.name = self.psql.name
-
-    def keep_alive(self):
-        self.etcd.write(self.name, self.postgresql.connection_string)
-
-    def initialize(self, force_leader=False):
-        self.keep_alive()
-
-        # is data directory empty?
-        if not self.psql.data_directory_empty():
-            self.load_psql()
-        elif not self.init_cluster(force_leader):
-            self.sync_from_leader()
-
-    def init_cluster(self, force_leader=False):
-        try:
-            self.etcd.init_cluster(self.INIT_KEY, self.name) or force_leader:
-        except etcd.EtcdAlreadyExist:
-            return False
-        else:
-            self.postgresql.initialize()
-            self.etcd.take_leadership(self.LEADER_KEY, self.name)
-            self.postgresql.start()
-            self.postgresql.create_users()
-            return True
-
-    def sync_from_leader(self):
-        while True:
-            logging.info('resolving leader')
-            try:
-                cluster = self.etcd.get_cluster()
-            except etcd.EtcdKeyNotFound:
-                continue
-
-            if not cluster.leader:
-                continue
-
-            logging.info('syncing with leader')
-            self.psql.sync_from_leader(cluster.leader)
-            self.psql.write_recovery_conf(cluster.leader)
-            self.psql.start()
-            return True
-
-    def load_psql(self):
-        if self.psql.is_running():
-            self.psql.load_replication_slots()
-
-    def run(self):
-        while True:
-            self.keep_alive()
-            logging.info(self.ha.run_cycle())
-            time.sleep(self.nap_time)
-
-    def cleanup(self):
-        self.psql.stop()
-        self.etcd.delete(self.name)
-        try:
-            self.etcd.delete(self.LEADER_KEY, self.name, prevValue=self.name)
-        except etcd.EtcdCompareFailed:
-            pass
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
---
+++ governor/__init__.py
@@ -0,0 +1,76 @@
+import logging
+import time
+
+import etcd
+
+from governor.etcd import Client as Etcd
+from governor.postgresql import Postgresql
+from governor.ha import Ha
+
+class Governor:
+    def __init__(self, config):
+        self.nap_time = config['loop_wait']
+
+        logging.info('waiting on etcd')
+        self.etcd = Etcd()
+        self.psql = Postgresql(config['postgresql'])
+        self.ha = Ha(self.psql, self.etcd)
+        self.name = self.psql.name
+
+    def keep_alive(self):
+        self.etcd.write(self.name, self.postgresql.connection_string)
+
+    def initialize(self, force_leader=False):
+        self.keep_alive()
+
+        # is data directory empty?
+        if not self.psql.data_directory_empty():
+            self.load_psql()
+        elif not self.init_cluster(force_leader):
+            self.sync_from_leader()
+
+    def init_cluster(self, force_leader=False):
+        try:
+            self.etcd.init_cluster(self.INIT_KEY, self.name) or force_leader:
+        except etcd.EtcdAlreadyExist:
+            return False
+        else:
+            self.postgresql.initialize()
+            self.etcd.take_leadership(self.LEADER_KEY, self.name)
+            self.postgresql.start()
+            self.postgresql.create_users()
+            return True
+
+    def sync_from_leader(self):
+        while True:
+            logging.info('resolving leader')
+            try:
+                cluster = self.etcd.get_cluster()
+            except etcd.EtcdKeyNotFound:
+                cluster = None
+
+            if cluster and cluster.leader:
+                logging.info('syncing with leader')
+                if self.psql.sync_from_leader(cluster.leader):
+                    self.psql.write_recovery_conf(cluster.leader)
+                    self.psql.start()
+                    return True
+            time.sleep(5)
+
+    def load_psql(self):
+        if self.psql.is_running():
+            self.psql.load_replication_slots()
+
+    def run(self):
+        while True:
+            self.keep_alive()
+            logging.info(self.ha.run_cycle())
+            time.sleep(self.nap_time)
+
+    def cleanup(self):
+        self.psql.stop()
+        self.etcd.delete(self.name)
+        try:
+            self.etcd.delete(self.LEADER_KEY, self.name, prevValue=self.name)
+        except etcd.EtcdCompareFailed:
+            pass
